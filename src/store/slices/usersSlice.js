410bebf47990f15a020df7887989c423b8dc3c89
federico-init <fefe.borrometi@gmail.com>
2024-12-06 08:57:28 +0100

    chore: add frist async thunk to fetch users update users initial state

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,12 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
-    // ...
+    isLoading: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return {
+        ...state,
+        isLoading: false,
+        users: action.payload,
+      };
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error,
+      };
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
