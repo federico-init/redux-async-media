226608d4ecf5c864d7be3e0136f7a25babcd0d49
federico-init <fefe.borrometi@gmail.com>
2024-12-07 19:38:18 +0100

    feat(usersList): add conditional rendering according to users fetch add skeleton loader(to be styled)

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
+import Skeleton from "./Skeleton";
+
 function UsersList() {
-  return <div>UsersList</div>;
+  const dispatch = useDispatch();
+
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (isLoading) {
+    return <Skeleton times={6} />;
+  }
+
+  if (error) {
+    return <div>Error fetching data!</div>;
+  }
+
+  return <div>Number of users: {data.length}</div>;
 }
 
 export default UsersList;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,16 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
+  await pause(1000);
+
   return response.data;
 });
+
+// TODO: remove this once project is done
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
