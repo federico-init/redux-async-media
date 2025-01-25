6624a44f503e41b93949b7dbb561145578927781
federico-init <fefe.borrometi@gmail.com>
2025-01-25 15:35:44 +0100

    chore: configure and implement first RTQ api

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,11 @@
+import { useFetchAlbumsQuery } from "../store";
+
 function AlbumsList({ user }) {
+  const res = useFetchAlbumsQuery(user);
+
+  // console.log(data, error, isLoading);
+  console.log(res);
+
   return <div>Albums by {user.name}</div>;
 }
 
